c0cf840e651f02c246d844561e3dc25f760b1386
ehmaddd <ehmaddd@gmail.com>
2023-06-15 21:54:54 +0500

    Add enumerables with class

---
+++ MyList.rb
@@ -0,0 +1,27 @@
+require_relative "MyEnumerable.rb"
+
+class MyList
+  include MyEnumerable
+  @list
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.my_all? {|e| e < 5}
+puts list.my_all? {|e| e > 5}
+
+# Test #any?
+puts list.my_any? {|e| e == 2}
+puts list.my_any? {|e| e == 5}
+
+#Test filter
+print list.my_filter {|e| e.even?}
